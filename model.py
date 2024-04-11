f6cb0c9d8213f031d6793ddbdbfece755e1858ea
MarcusCCH <marcuscch12@gmail.com>
2024-04-11 23:30:48 +0800

    update

---
+++ model.py
@@ -0,0 +1,43 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+
+class LeNet(nn.Module):
+    def __init__(self, num_traits):
+        super(LeNet, self).__init__()
+        img_shape = 512
+        self.conv1 = nn.Conv2d(3, 6, kernel_size=5)
+        self.conv2 = nn.Conv2d(6, 16, kernel_size=5)
+        self.fc1 = nn.Linear(16 * 4 * 4, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, num_traits)
+
+    def forward(self, x):
+        x = torch.relu(self.conv1(x))
+        x = F.avg_pool2d(x, kernel_size=2)
+        x = torch.relu(self.conv2(x))
+        x = F.avg_pool2d(x, kernel_size=2)
+        x = x.view(x.size(0), -1)
+        x = torch.relu(self.fc1(x))
+        x = torch.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+class ResNet(nn.Module):
+    def __init__(self, num_channel_in=16*3, class_num=None):
+        super(ResNet, self).__init__()
+        self.c1 = nn.Conv2d(num_channel_in, 128, kernel_size=3, stride=1, padding=1)
+        self.c2 = nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1)
+        self.c3 = nn.Conv2d(256, 512, kernel_size=3, stride=1, padding=1)
+        self.gap = nn.AdaptiveAvgPool2d((1, 1))
+        self.linear = torch.nn.Linear(512, class_num)
+
+    def forward(self, x):
+        x = self.c1(x)
+        x = self.c2(x)
+        x = self.c3(x)
+        x = self.gap(x)
+        x = torch.flatten(x, 1)
+        x = self.linear(x)
+        return x
--- utils/data.py
+++ utils/data.py
@@ -2,6 +2,9 @@ import pandas as pd
 from tqdm import tqdm
 from random import shuffle, seed
 import os
+import torch
+import torchvision
+import numpy as np
 def get_training_data(train_path, image_path):
     assert "csv" in train_path
     try:
@@ -10,30 +13,59 @@ def get_training_data(train_path, image_path):
         raise Exception("Error loading training data at path: {train_path}")
 
     cases = []
-    for case in tqdm(df[["id"]].iterrows()):
-        id = case[1]["id"]
-        case = PlantCase(id, image_path)
+    # for case in tqdm(df[["id"]].iterrows()):
+    #     id = case[1]["id"]
+    #     case = PlantCase(id, image_path)
+    #     cases.append(case)
+
+    attr_list = "id,X4_mean,X11_mean,X18_mean,X26_mean,X50_mean,X3112_mean,X4_sd,X11_sd,X18_sd,X26_sd,X50_sd,X3112_sd".split(",")
+    print(attr_list)
+    for case in tqdm(df.iterrows()):
+        data = {}
+        for attr in attr_list: 
+            # id = case[1]["id"]
+            data[attr] = case[1][attr]
+        case = PlantCase(data, image_path)
         cases.append(case)
     
+    cases[0].pprint()
     shuffle(cases)
 
     return cases
 
 class PlantCase:
-    def __init__(self, id, image_path):
-        self.id = id;
-        self.image = os.path.join(image_path, f"{id}.jpeg")
+    def __init__(self, data, image_path):
+
+        self.id =int(data["id"])
+        self.image_url = os.path.join(image_path, f"{self.id}.jpeg")
+        self.image =  torchvision.io.read_image(self.image_url)
+
+        self.trait_mean = [v for index,(k,v) in enumerate(data.items()) if "mean" in k]
+        self.trait_sd = [v for index,(k,v) in enumerate(data.items()) if "sd" in k]   
+
         
-        # extra data 
+    def pprint(self):
+
+        # print(self.image)
+        print(self.trait_mean)
+        print(self.trait_sd)
         
 class CaseCollator:
     def __call__(self, cases):
         # print(len(cases))
         # print(type(cases))
-        data_list = []
+        imgs = []
+        mean = []
+        sd = []
         for case in cases:
-            data_list.append(case.image)
-        return data_list
+            img = case.image.cpu().detach().numpy()
+            imgs.append(img)
+            mean.append(case.trait_mean)
+            sd.append(case.trait_sd)
+  
+        # return (torch.Tensor([item.cpu().detach().numpy() for item in imgs]), torch.Tensor(mean),torch.Tensor(sd))
+        return (torch.Tensor(np.array(imgs)), torch.Tensor(mean),torch.Tensor(sd))
+
         
            
 
@@ -41,4 +73,4 @@ if __name__ == "__main__":
     # list = "WORLDCLIM_BIO1_annual_mean_temperature,WORLDCLIM_BIO12_annual_precipitation,WORLDCLIM_BIO13.BIO14_delta_precipitation_of_wettest_and_dryest_month,WORLDCLIM_BIO15_precipitation_seasonality,WORLDCLIM_BIO4_temperature_seasonality,WORLDCLIM_BIO7_temperature_annual_range,SOIL_bdod_0.5cm_mean_0.01_deg,SOIL_bdod_100.200cm_mean_0.01_deg,SOIL_bdod_15.30cm_mean_0.01_deg,SOIL_bdod_30.60cm_mean_0.01_deg,SOIL_bdod_5.15cm_mean_0.01_deg,SOIL_bdod_60.100cm_mean_0.01_deg,SOIL_cec_0.5cm_mean_0.01_deg,SOIL_cec_100.200cm_mean_0.01_deg,SOIL_cec_15.30cm_mean_0.01_deg,SOIL_cec_30.60cm_mean_0.01_deg,SOIL_cec_5.15cm_mean_0.01_deg,SOIL_cec_60.100cm_mean_0.01_deg,SOIL_cfvo_0.5cm_mean_0.01_deg,SOIL_cfvo_100.200cm_mean_0.01_deg,SOIL_cfvo_15.30cm_mean_0.01_deg,SOIL_cfvo_30.60cm_mean_0.01_deg,SOIL_cfvo_5.15cm_mean_0.01_deg,SOIL_cfvo_60.100cm_mean_0.01_deg,SOIL_clay_0.5cm_mean_0.01_deg,SOIL_clay_100.200cm_mean_0.01_deg,SOIL_clay_15.30cm_mean_0.01_deg,SOIL_clay_30.60cm_mean_0.01_deg,SOIL_clay_5.15cm_mean_0.01_deg,SOIL_clay_60.100cm_mean_0.01_deg,SOIL_nitrogen_0.5cm_mean_0.01_deg,SOIL_nitrogen_100.200cm_mean_0.01_deg,SOIL_nitrogen_15.30cm_mean_0.01_deg,SOIL_nitrogen_30.60cm_mean_0.01_deg,SOIL_nitrogen_5.15cm_mean_0.01_deg,SOIL_nitrogen_60.100cm_mean_0.01_deg,SOIL_ocd_0.5cm_mean_0.01_deg,SOIL_ocd_100.200cm_mean_0.01_deg,SOIL_ocd_15.30cm_mean_0.01_deg,SOIL_ocd_30.60cm_mean_0.01_deg,SOIL_ocd_5.15cm_mean_0.01_deg,SOIL_ocd_60.100cm_mean_0.01_deg,SOIL_ocs_0.30cm_mean_0.01_deg,SOIL_phh2o_0.5cm_mean_0.01_deg,SOIL_phh2o_100.200cm_mean_0.01_deg,SOIL_phh2o_15.30cm_mean_0.01_deg,SOIL_phh2o_30.60cm_mean_0.01_deg,SOIL_phh2o_5.15cm_mean_0.01_deg,SOIL_phh2o_60.100cm_mean_0.01_deg,SOIL_sand_0.5cm_mean_0.01_deg,SOIL_sand_100.200cm_mean_0.01_deg,SOIL_sand_15.30cm_mean_0.01_deg,SOIL_sand_30.60cm_mean_0.01_deg,SOIL_sand_5.15cm_mean_0.01_deg,SOIL_sand_60.100cm_mean_0.01_deg,SOIL_silt_0.5cm_mean_0.01_deg,SOIL_silt_100.200cm_mean_0.01_deg,SOIL_silt_15.30cm_mean_0.01_deg,SOIL_silt_30.60cm_mean_0.01_deg,SOIL_silt_5.15cm_mean_0.01_deg,SOIL_silt_60.100cm_mean_0.01_deg,SOIL_soc_0.5cm_mean_0.01_deg,SOIL_soc_100.200cm_mean_0.01_deg,SOIL_soc_15.30cm_mean_0.01_deg,SOIL_soc_30.60cm_mean_0.01_deg,SOIL_soc_5.15cm_mean_0.01_deg,SOIL_soc_60.100cm_mean_0.01_deg,MODIS_2000.2020_monthly_mean_surface_reflectance_band_01_._month_m1,MODIS_2000.2020_monthly_mean_surface_reflectance_band_02_._month_m1,MODIS_2000.2020_monthly_mean_surface_reflectance_band_03_._month_m1,MODIS_2000.2020_monthly_mean_surface_reflectance_band_04_._month_m1,MODIS_2000.2020_monthly_mean_surface_reflectance_band_05_._month_m1,MODIS_2000.2020_monthly_mean_surface_reflectance_band_01_._month_m10,MODIS_2000.2020_monthly_mean_surface_reflectance_band_02_._month_m10,MODIS_2000.2020_monthly_mean_surface_reflectance_band_03_._month_m10,MODIS_2000.2020_monthly_mean_surface_reflectance_band_04_._month_m10,MODIS_2000.2020_monthly_mean_surface_reflectance_band_05_._month_m10,MODIS_2000.2020_monthly_mean_surface_reflectance_band_01_._month_m11,MODIS_2000.2020_monthly_mean_surface_reflectance_band_02_._month_m11,MODIS_2000.2020_monthly_mean_surface_reflectance_band_03_._month_m11,MODIS_2000.2020_monthly_mean_surface_reflectance_band_04_._month_m11,MODIS_2000.2020_monthly_mean_surface_reflectance_band_05_._month_m11,MODIS_2000.2020_monthly_mean_surface_reflectance_band_01_._month_m12,MODIS_2000.2020_monthly_mean_surface_reflectance_band_02_._month_m12,MODIS_2000.2020_monthly_mean_surface_reflectance_band_03_._month_m12,MODIS_2000.2020_monthly_mean_surface_reflectance_band_04_._month_m12,MODIS_2000.2020_monthly_mean_surface_reflectance_band_05_._month_m12,MODIS_2000.2020_monthly_mean_surface_reflectance_band_01_._month_m2,MODIS_2000.2020_monthly_mean_surface_reflectance_band_02_._month_m2,MODIS_2000.2020_monthly_mean_surface_reflectance_band_03_._month_m2,MODIS_2000.2020_monthly_mean_surface_reflectance_band_04_._month_m2,MODIS_2000.2020_monthly_mean_surface_reflectance_band_05_._month_m2,MODIS_2000.2020_monthly_mean_surface_reflectance_band_01_._month_m3,MODIS_2000.2020_monthly_mean_surface_reflectance_band_02_._month_m3,MODIS_2000.2020_monthly_mean_surface_reflectance_band_03_._month_m3,MODIS_2000.2020_monthly_mean_surface_reflectance_band_04_._month_m3,MODIS_2000.2020_monthly_mean_surface_reflectance_band_05_._month_m3,MODIS_2000.2020_monthly_mean_surface_reflectance_band_01_._month_m4,MODIS_2000.2020_monthly_mean_surface_reflectance_band_02_._month_m4,MODIS_2000.2020_monthly_mean_surface_reflectance_band_03_._month_m4,MODIS_2000.2020_monthly_mean_surface_reflectance_band_04_._month_m4,MODIS_2000.2020_monthly_mean_surface_reflectance_band_05_._month_m4,MODIS_2000.2020_monthly_mean_surface_reflectance_band_01_._month_m5,MODIS_2000.2020_monthly_mean_surface_reflectance_band_02_._month_m5,MODIS_2000.2020_monthly_mean_surface_reflectance_band_03_._month_m5,MODIS_2000.2020_monthly_mean_surface_reflectance_band_04_._month_m5,MODIS_2000.2020_monthly_mean_surface_reflectance_band_05_._month_m5,MODIS_2000.2020_monthly_mean_surface_reflectance_band_01_._month_m6,MODIS_2000.2020_monthly_mean_surface_reflectance_band_02_._month_m6,MODIS_2000.2020_monthly_mean_surface_reflectance_band_03_._month_m6,MODIS_2000.2020_monthly_mean_surface_reflectance_band_04_._month_m6,MODIS_2000.2020_monthly_mean_surface_reflectance_band_05_._month_m6,MODIS_2000.2020_monthly_mean_surface_reflectance_band_01_._month_m7,MODIS_2000.2020_monthly_mean_surface_reflectance_band_02_._month_m7,MODIS_2000.2020_monthly_mean_surface_reflectance_band_03_._month_m7,MODIS_2000.2020_monthly_mean_surface_reflectance_band_04_._month_m7,MODIS_2000.2020_monthly_mean_surface_reflectance_band_05_._month_m7,MODIS_2000.2020_monthly_mean_surface_reflectance_band_01_._month_m8,MODIS_2000.2020_monthly_mean_surface_reflectance_band_02_._month_m8,MODIS_2000.2020_monthly_mean_surface_reflectance_band_03_._month_m8,MODIS_2000.2020_monthly_mean_surface_reflectance_band_04_._month_m8,MODIS_2000.2020_monthly_mean_surface_reflectance_band_05_._month_m8,MODIS_2000.2020_monthly_mean_surface_reflectance_band_01_._month_m9,MODIS_2000.2020_monthly_mean_surface_reflectance_band_02_._month_m9,MODIS_2000.2020_monthly_mean_surface_reflectance_band_03_._month_m9,MODIS_2000.2020_monthly_mean_surface_reflectance_band_04_._month_m9,MODIS_2000.2020_monthly_mean_surface_reflectance_band_05_._month_m9,VOD_C_2002_2018_multiyear_mean_m01,VOD_C_2002_2018_multiyear_mean_m02,VOD_C_2002_2018_multiyear_mean_m03,VOD_C_2002_2018_multiyear_mean_m04,VOD_C_2002_2018_multiyear_mean_m05,VOD_C_2002_2018_multiyear_mean_m06,VOD_C_2002_2018_multiyear_mean_m07,VOD_C_2002_2018_multiyear_mean_m08,VOD_C_2002_2018_multiyear_mean_m09,VOD_C_2002_2018_multiyear_mean_m10,VOD_C_2002_2018_multiyear_mean_m11,VOD_C_2002_2018_multiyear_mean_m12,VOD_Ku_1987_2017_multiyear_mean_m01,VOD_Ku_1987_2017_multiyear_mean_m02,VOD_Ku_1987_2017_multiyear_mean_m03,VOD_Ku_1987_2017_multiyear_mean_m04,VOD_Ku_1987_2017_multiyear_mean_m05,VOD_Ku_1987_2017_multiyear_mean_m06,VOD_Ku_1987_2017_multiyear_mean_m07,VOD_Ku_1987_2017_multiyear_mean_m08,VOD_Ku_1987_2017_multiyear_mean_m09,VOD_Ku_1987_2017_multiyear_mean_m10,VOD_Ku_1987_2017_multiyear_mean_m11,VOD_Ku_1987_2017_multiyear_mean_m12,VOD_X_1997_2018_multiyear_mean_m01,VOD_X_1997_2018_multiyear_mean_m02,VOD_X_1997_2018_multiyear_mean_m03,VOD_X_1997_2018_multiyear_mean_m04,VOD_X_1997_2018_multiyear_mean_m05,VOD_X_1997_2018_multiyear_mean_m06,VOD_X_1997_2018_multiyear_mean_m07,VOD_X_1997_2018_multiyear_mean_m08,VOD_X_1997_2018_multiyear_mean_m09,VOD_X_1997_2018_multiyear_mean_m10,VOD_X_1997_2018_multiyear_mean_m11,VOD_X_1997_2018_multiyear_mean_m12"
     # print(list.split(","))
     
-    get_training_data("data/test.csv", "data/train_images")
+    get_training_data("sample.csv", "data/test_images")
